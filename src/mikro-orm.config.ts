6eb9bbe88b0104eddca23519b62dcb73aacaaed0
Rrahul Raja <rrahul@Rrahuls-MacBook-Pro.local>
2021-06-16 16:07:14 +0530

    Project structure setup

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,12 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import {MikroORM} from "@mikro-orm/core"
+
+export default {
+    entities: [Post],
+    dbName: 'lireddit',
+    user: 'postgres',
+    password: 'postgres',
+    type: 'postgresql',
+    debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
