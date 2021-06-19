d8e3c76001879eea872fa67baffefe075ae0f14d
Rrahul Raja <rrahul@Rrahuls-MacBook-Pro.local>
2021-06-19 13:59:52 +0530

    Added email field, refactored code for email field and setup sending email using nodemailer

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,7 @@ import session from "express-session";
 import connectRedis from "connect-redis";
 import { MyContext } from "./types";
 import cors from "cors";
+import { sendMail } from "./utils/sendMail";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -3,7 +3,6 @@ import { MyContext } from "src/types";
 import {
   Resolver,
   Mutation,
-  InputType,
   Field,
   Arg,
   Ctx,
@@ -12,15 +11,9 @@ import {
 } from "type-graphql";
 import argon2 from "argon2";
 import { COOKIE_NAME } from "../constants";
-
-@InputType()
-class UsernamePasswordInput {
-  @Field()
-  username: string;
-
-  @Field()
-  password: string;
-}
+import { UsernamePasswordInput } from "./UsernamePasswordInput";
+import { validateRegister } from "../utils/validateRegister";
+import { sendMail } from "../utils/sendMail";
 
 @ObjectType()
 class FieldError {
@@ -42,6 +35,19 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Mutation(() => Boolean)
+  async forgotPassword(@Arg("email") email: string, @Ctx() { em }: MyContext) {
+    const user = await em.findOne(User, { email });
+    if(!user) {
+      // the email is not in the db
+      return true
+    }
+
+    const token = 'fjaskaksfrewrewq';    
+    sendMail(email, `<a href="http://localhost:3000/change-password/${token}">reset password</a>`);
+    return true;
+  }
+
   @Query(() => User, { nullable: true })
   async me(@Ctx() { req, em }: MyContext) {
     if (!req.session.userId) {
@@ -55,34 +61,18 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em, req}: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
-    if (options.username.length <= 2) {
-      return {
-        errors: [
-          {
-            field: "username",
-            message: "length must be greater than 2",
-          },
-        ],
-      };
-    }
-
-    if (options.password.length <= 3) {
-      return {
-        errors: [
-          {
-            field: "password",
-            message: "length must be greater than 3",
-          },
-        ],
-      };
+    const errors = validateRegister(options);
+    if (errors) {
+      return { errors };
     }
 
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword,
+      email: options.email,
     });
     try {
       await em.persistAndFlush(user);
@@ -110,22 +100,28 @@ export class UserResolver {
 
   @Mutation(() => UserResponse)
   async login(
-    @Arg("options") options: UsernamePasswordInput,
+    @Arg("usernameOrEmail") usernameOrEmail: string,
+    @Arg("password") password: string,
     @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
-    const user = await em.findOne(User, { username: options.username });
+    const user = await em.findOne(
+      User,
+      usernameOrEmail.includes("@")
+        ? { email: usernameOrEmail }
+        : { username: usernameOrEmail }
+    );
     if (!user) {
       return {
         errors: [
           {
-            field: "username",
+            field: "usernameOrEmail",
             message: "that username doesn't exists",
           },
         ],
       };
     }
 
-    const valid = await argon2.verify(user.password, options.password);
+    const valid = await argon2.verify(user.password, password);
     if (!valid) {
       return {
         errors: [
@@ -143,15 +139,17 @@ export class UserResolver {
   }
 
   @Mutation(() => Boolean)
-  logout(@Ctx() {req, res}:MyContext) {
-    return new Promise((resolve) => req.session.destroy((err) => {
-      res.clearCookie(COOKIE_NAME);
-      if(err) {
-        console.log(err);
-        return resolve(false);
-      }
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          return resolve(false);
+        }
 
-      return resolve(true);
-    }))
+        return resolve(true);
+      })
+    );
   }
 }
